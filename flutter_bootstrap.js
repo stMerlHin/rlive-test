d7126e9ee718394b4228778053a973de39b629cd
st <stmerlhin@gmail.com>
2026-06-15 19:47:29 +0000

    add user intent page

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3829746123" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3867247696" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
